a94383f086cf10b8a1ed2372822d76feac1a1ab6
gabriel7mendes <andrewsseven@gmail.com>
2021-02-18 10:44:28 -0300

    first commit

---
+++ src/com/balazscholczer/udemy/App.java
@@ -0,0 +1,14 @@
+package com.balazscholczer.udemy;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class App {
+	
+	public static void main(String[] args) {
+		ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors());
+		
+		SimpleRecursiveTask task = new SimpleRecursiveTask(120);
+		System.out.println(pool.invoke(task));
+	}
+
+}
